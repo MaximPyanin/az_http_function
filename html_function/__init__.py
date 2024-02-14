c306ccab499134a63894899ad67cf9945626dd8a
Maxim Pyanin <maximpyanin@icloud.com>
2024-02-14 22:01:14 +0300

    Update __init__.py

--- html_function/__init__.py
+++ html_function/__init__.py
@@ -284,6 +284,7 @@ main {
 }
  </style>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+  <link rel="stylesheet" href="с.css">
   <title>Document</title>
 </head>
 <body>
