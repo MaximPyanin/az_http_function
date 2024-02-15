67530432ccc896e7ba4eac11c8ffb0a1e6c6939d
Maxim Pyanin <maximpyanin@icloud.com>
2024-02-15 15:46:18 +0300

    Update __init__.py

--- html_function/__init__.py
+++ html_function/__init__.py
@@ -10,341 +10,337 @@ def main(req:func.HttpRequest) -> func.HttpResponse:
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <style> 
-  body {
-  background-color: #edf5ff;
-  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, Segoe UI Symbol;
-  font-size: 14px;
+ body {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+.content .column > .pic {
+  background-color: #a2a285;
+  border-radius: 1.5625rem;
+}
+.content .column:first-of-type > .pic:first-of-type {
+  height: 14rem;
+}
+.content .column:first-of-type > .pic:nth-of-type(2) {
+  height: 20rem;
+}
+.content .column:first-of-type > .pic:nth-of-type(3) {
+  height: 23rem;
+}
+.content .column:first-of-type > .pic:nth-of-type(4) {
+  height: 17rem;
+}
+.content .column:first-of-type > .pic:nth-of-type(5) {
+  height: 19rem;
+}
+.content .column:first-of-type > .pic:nth-of-type(6) {
+  height: 16rem;
+}
+.content .column:nth-of-type(2) > .pic:first-of-type {
+  height: 18rem;
+}
+.content .column:nth-of-type(2) > .pic:nth-of-type(2) {
+  height: 20rem;
+}
+.content .column:nth-of-type(2) > .pic:nth-of-type(3) {
+  height: 18rem;
+}
+.content .column:nth-of-type(2) > .pic:nth-of-type(4) {
+  height: 19rem;
+}
+.content .column:nth-of-type(2) > .pic:nth-of-type(5) {
+  height: 16rem;
+}
+.content .column:nth-of-type(2) > .pic:nth-of-type(6) {
+  height: 20rem;
+}
+.content .column:nth-of-type(3) > .pic:first-of-type {
+  height: 17rem;
+}
+.content .column:nth-of-type(3) > .pic:nth-of-type(2) {
+  height: 23rem;
+}
+.content .column:nth-of-type(3) > .pic:nth-of-type(3) {
+  height: 16rem;
+}
+.content .column:nth-of-type(3) > .pic:nth-of-type(4) {
+  height: 18rem;
+}
+.content .column:nth-of-type(3) > .pic:nth-of-type(5) {
+  height: 14rem;
+}
+.content .column:nth-of-type(3) > .pic:nth-of-type(6) {
+  height: 20rem;
+}
+.content .column:nth-of-type(4) > .pic:first-of-type {
+  height: 19rem;
+}
+.content .column:nth-of-type(4) > .pic:nth-of-type(2) {
+  height: 16rem;
+}
+.content .column:nth-of-type(4) > .pic:nth-of-type(3) {
+  height: 17rem;
+}
+.content .column:nth-of-type(4) > .pic:nth-of-type(4) {
+  height: 19rem;
+}
+.content .column:nth-of-type(4) > .pic:nth-of-type(5) {
+  height: 16rem;
+}
+.content .column:nth-of-type(4) > .pic:nth-of-type(6) {
+  height: 17rem;
+}
+.content .column:nth-of-type(5) > .pic:first-of-type {
+  height: 17rem;
+}
+.content .column:nth-of-type(5) > .pic:nth-of-type(2) {
+  height: 19rem;
+}
+.content .column:nth-of-type(5) > .pic:nth-of-type(3) {
+  height: 18rem;
+}
+.content .column:nth-of-type(5) > .pic:nth-of-type(4) {
+  height: 14rem;
+}
+.content .column:nth-of-type(5) > .pic:nth-of-type(5) {
+  height: 15rem;
+}
+.content .column:nth-of-type(5) > .pic:nth-of-type(6) {
+  height: 17rem;
+}
+.content .column:nth-of-type(6) > .pic:first-of-type {
+  height: 15rem;
+}
+.content .column:nth-of-type(6) > .pic:nth-of-type(2) {
+  height: 17rem;
+}
+.content .column:nth-of-type(6) > .pic:nth-of-type(3) {
+  height: 19rem;
+}
+.content .column:nth-of-type(6) > .pic:nth-of-type(4) {
+  height: 14rem;
+}
+.content .column:nth-of-type(6) > .pic:nth-of-type(5) {
+  height: 20rem;
+}
+.content .column:nth-of-type(6) > .pic:nth-of-type(6) {
+  height: 18rem;
 }
 
 header {
-  width: 1280px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  border-bottom: 2px solid grey;
   display: flex;
+  height: 7%;
   flex-direction: row;
-  justify-content: space-around;
-}
-
-.logo {
-  width: 140px;
-  height: 35px;
-}
-
-.menu {
-  font-weight: bold;
-  font-size: medium;
-  padding-left: 315px;
-  display: flex;
-  flex-direction: row;
-  -moz-column-gap: 25px;
-       column-gap: 25px;
-}
-.menu a {
-  text-decoration: none;
-  color: #191919;
-}
-.menu a:hover {
-  color: #1876d1;
-}
-
-.icons {
-  display: flex;
-  flex-direction: row;
-  -moz-column-gap: 12px;
-       column-gap: 12px;
-}
-.icons i:hover {
-  background-color: black;
-}
-.icons i {
-  background-color: white;
-  border: 1px solid grey;
-  border-radius: 8px;
-  height: 25px;
-  width: 20px;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
   align-items: center;
-}
-
-.buttons {
-  padding-top: 14px;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
   -moz-column-gap: 10px;
        column-gap: 10px;
 }
 
-.b1 {
-  background-color: blue;
-  color: white;
+form {
+  padding-left: 20px;
+  width: 1200px;
   border-radius: 10px;
-  height: 47px;
-  width: 120px;
+  background-color: grey;
+}
+form input {
+  background-color: grey;
+  color: black;
   border: 0;
-  font-weight: bold;
+  width: 100px;
 }
 
-.b2 {
+header .fab {
+  margin-left: 12px;
+  background-color: white;
+  color: red;
+  border-radius: 12px;
+}
+
+header button {
+  color: white;
+  background-color: black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   border-radius: 10px;
-  height: 47px;
-  width: 120px;
-  background-color: #edf5ff;
-  color: #191919;
+}
+
+header .right {
+  display: flex;
+  flex-direction: row;
+  -moz-column-gap: 14px;
+       column-gap: 14px;
+}
+header .right i {
+  color: rgba(79, 77, 77, 0.699);
+}
+
+.header2 {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  border-radius: 10px;
+  -moz-column-gap: 15px;
+       column-gap: 15px;
+  border: 2px solid gray;
+  margin-top: 80px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 90%;
+}
+.header2 button {
+  border-radius: 10px;
+  height: 45px;
+  width: 140px;
+  color: black;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  font-size: smaller;
+  border: 0;
+}
+.header2 p {
+  padding-left: 9px;
+}
+.header2 p a {
+  text-decoration: none;
+  color: black;
   font-weight: bold;
-  border: 1px solid grey;
 }
 
-.item1 {
-  height: 170px;
-  width: 170px;
-  background-color: white;
-  border: 1px solid black;
-  border-radius: 8px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-.item1 p {
-  margin: 0;
-}
-
-.item1:hover {
-  background-color: #e3f0ff;
-}
-
-.item2 {
-  height: 170px;
-  width: 170px;
-  background-color: white;
-  border: 1px solid black;
-  border-radius: 8px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-.item2 p {
-  margin: 0;
-}
-
-.item2:hover {
-  background-color: #e3f0ff;
-}
-
-.item3 {
-  height: 170px;
-  width: 170px;
-  background-color: white;
-  border: 1px solid black;
-  border-radius: 8px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-.item3 p {
-  margin: 0;
-}
-
-.item3:hover {
-  background-color: #e3f0ff;
-}
-
-.item4 {
-  height: 170px;
-  width: 170px;
-  background-color: white;
-  border: 1px solid black;
-  border-radius: 8px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-.item4 p {
-  margin: 0;
-}
-
-.item4:hover {
-  background-color: #e3f0ff;
-}
-
-.item5 {
-  height: 170px;
-  width: 170px;
-  background-color: white;
-  border: 1px solid black;
-  border-radius: 8px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-.item5 p {
-  margin: 0;
-}
-
-.item5:hover {
-  background-color: #e3f0ff;
-}
-
-.item6 {
-  height: 170px;
-  width: 170px;
-  background-color: white;
-  border: 1px solid black;
-  border-radius: 8px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-.item6 p {
-  margin: 0;
-}
-
-.item6:hover {
-  background-color: #e3f0ff;
-}
-
-.item7 {
-  height: 170px;
-  width: 170px;
-  background-color: white;
-  border: 1px solid black;
-  border-radius: 8px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-.item7 p {
-  margin: 0;
-}
-
-.item7:hover {
-  background-color: #e3f0ff;
-}
-
-.items {
-  padding-left: 450px;
-  padding-top: 50px;
-  padding-right: 400px;
-}
-
-main {
-  position: relative;
-}
-
-.line {
-  position: absolute;
-  z-index: -1;
-  background-color: #73879e;
-  width: 100%;
-  top: 340px;
-  height: 100px;
-}
-
-.items {
+.content {
+  padding-top: 55px;
+  padding-left: 95px;
+  padding-right: 95px;
   display: grid;
-  -moz-column-gap: 20px;
-       column-gap: 20px;
-  row-gap: 20px;
-  grid-template-areas: ". b ." "a b c" "a d c" "f d e" "f g e" ". g .";
+  grid-template-columns: repeat(6, 1fr);
+  -moz-column-gap: 18px;
+       column-gap: 18px;
 }
-
-.item1 {
-  grid-area: a;
-}
-
-.item2 {
-  grid-area: b;
-}
-
-.item3 {
-  grid-area: c;
-}
-
-.item4 {
-  grid-area: d;
-}
-
-.item5 {
-  grid-area: e;
-}
-
-.item6 {
-  grid-area: f;
-}
-
-.item7 {
-  grid-area: g;
-}
+.content .column {
+  display: grid;
+  row-gap: 9px;
+}/*# sourceMappingURL=style (2).css.map */
  </style>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
   <title>Document</title>
 </head>
-<body>
 <header>
-<img src="logo.svg" class="logo">
-<div class="menu">
-<a href="#">Components</a>
-<a href="#">Blocks</a>
-<a href="#">Designer</a>
-<a href="#">Templates</a>
-</div>
-<div class="icons">
-  <i class="fa-brands fa-github"></i>
-  <i class="fa-brands fa-discord"></i>
-  <i class="fa-regular fa-moon"></i>
-</div>
-</header>
+    <i class="fab fa-pinterest"></i>
+    <button>Glowna Strona</button>
+    
+    <form action="/search" method="get">
+      <input type="search" name="q" placeholder="szukaj">
+    </form>
+ 
+    <div class="right">
+      <i class="fa-solid fa-bell"></i>
+      <i class="fa-solid fa-comment"></i>
+    </div>
+  </header>
 <main>
-<div class="items">
-<div class="item1">
-<img src="icon1.svg">
-<h3>Templates</h3>
-<p>Spectacular designer</p>
+<div class="header2">
+<p>Używamy plików cookie, aby wyświetlać spersonalizowane reklamy i treści oraz analizować ruch w naszej witrynie. Więcej<br> informacji znajdziesz w naszym dokumencie <a href="#">Zasady dotyczące plików cookie.</a>
+</p>
+  <button>Zaakceptuj wszystkie</button>
+  <button>Odrzuć wszystkie</button>
+  <button>Chcę wybrać samodzielnie</button>  
 </div>
-<div class="item2">
-  <img src="icon2.svg">
-  <h3>Open Source</h3>
-  <p>90+ UI Components</p>
+<div class="content">
+  <div class="column">
+   <div class="pic">
+   </div>
+   <div class="pic">
+   </div>
+   <div class="pic">
+  </div>
+  <div class="pic">
+  </div>
+  <div class="pic">
+  </div>
+  <div class="pic">
+  </div>
+  </div>
+  <div class="column">
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+  </div>
+  <div class="column">
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+  </div>
+  <div class="column">
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+  </div>
+  <div class="column">
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+  </div>
+  <div class="column">
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+    <div class="pic">
+    </div>
+  </div>
 </div>
-<div class="item3">
-  <img src="icon3.svg">
-  <h3>Prime Blocks</h3>
-  <p>400+ UI Blocks</p>
-</div>
-<div class="item4">
-  <img src="icon4.svg">
-  <h3>NEXT-GEN</h3>
-  <p>ANGULAR UI</p>
-</div>
-<div class="item5">
-  <img src="icon5.svg">
-  <h3>Theme Designer</h3>
-  <p>Create Your Own</p>
-</div>
-<div class="item6">
-  <img src="icon6.svg">
-  <h3>CSS Utilities</h3>
-  <p>PrimeFlex CSS</p>
-</div>
-<div class="item7">
-  <img src="icon7.svg">
-  <h3>Icon Libraey</h3>
-  <p>200+ Icons</p>
-</div>
-</div>
-<div class="line"></div>
 </main>
-<div class="buttons">
-<button class="b1">Get Started</button>
-<button class="b2">npm i primeng</button>
-</div>
 </body>
 </html>''',
         status_code=200,
